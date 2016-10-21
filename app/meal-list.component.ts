b5d5c3a685f5ebe9f4b667fd55d4fb400245f091
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-21 08:36:50 -0700

    add eventemitter to meal-list for editting meals

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Meal } from './meal.model';
 
 @Component ({
@@ -6,10 +6,13 @@ import { Meal } from './meal.model';
   template: `
   <div class ="meal" *ngFor="let currentMeal of childMealList">
   <meal-display [meal]="currentMeal"></meal-display>
+  <button class="btn btn-default"(click)="editButtonHasBeenClicked(currentTask)" >Edit</button>
   </div>
   `
 })
 
 export class MealListComponent {
-  
+  @Input() childMealList: Meal[];
+  @Output() clickSender = new EventEmitter();
+
 }
