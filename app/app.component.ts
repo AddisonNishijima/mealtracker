2d9038cce0b78d203375aa1e2069b2aa5f45a71c
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-21 07:54:34 -0700

    add basic html to app component page

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+  <div>
   <h1>Meal Tracker</h1>
+  </div>
+  <div class="col-md-8">
+  <h1>Meals I've Had Today</h1>
+  </div>
+  <div class="col-md-4">
+  <h1>Add a Meal</h1>
+  </div>
   `
 })
 
