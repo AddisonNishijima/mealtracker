2c19f62746269b0b7f5ae6c1a38f21f9f68adb2a
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-21 08:13:10 -0700

    add MealComponent

---
+++ app/meal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component ({
+  selector: 'meal-display',
+  template: `
+  <div>
+  </div>
+  `
+})
+export class MealComponent {
+
+}
