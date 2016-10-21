8adf150a5aabb54eedc877416b84a2c83b6f7e0a
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-21 11:02:54 -0700

    add calorie pipe

---
+++ app/calorie.pipe.ts
@@ -0,0 +1,27 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import { Meal } from './meal.model';
+
+@Pipe ({
+  name: "calorie",
+  pure: false
+})
+export class CaloriePipe implements PipeTransform {
+  transform(input: Meal[], desiredCalorie) {
+    var output: Meal[] = [];
+    if(desiredCalorie === "highcalorie") {
+      for (var i=0; i < input.length; i++) {
+        if (input[i].calories >= 500) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }else if (desiredCalorie === "all") {
+      for (var i=0; i < input.length; i++) {
+        if (input[i].calories < 500) {
+          output.push(input[i]);
+        }
+      }
+      return output
+    }
+  }
+}
