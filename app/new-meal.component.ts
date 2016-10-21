1dc939824a927306cda1f6e4fc8650b285c00b52
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-21 09:06:13 -0700

    add new meal component

---
+++ app/new-meal.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component ({
+  selector: 'new-meal',
+  template: `
+  <div *ngIf="childSelectedMeal">
+  <h1>New Meal</h1>
+    <div>
+      <div class="form-group">
+        <label>Enter New Meal Name</label>
+        <input class="form-control" #newName>
+      </div>
+      <div class="form-group">
+        <label>Enter New Meal Details</label>
+        <input class="form-control" #newDetails>
+      </div>
+      <div class="form-group">
+        <label>Enter New Meal Calories</label>
+        <input type="number" class="form-control" #newCalories>
+      </div>
+      <button class="btn btn-success" (click)="
+      addClicked(newName.value, newDetails.value, newCalories.value)">Add</button>
+    </div>
+  </div>
+  `
+})
+export class EditMealComponent {
+  @Output() newMealSender = new EventEmitter();
+  addClicked(name: string, details: string, calories: number) {
+    var newMealToAdd: Meal = new Meal(name, details, calories);
+    this.newMealSender.emit(newMealToAdd);
+  }
+}
